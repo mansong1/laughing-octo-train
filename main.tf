b6d4959b9e903c99a0788515257b335a9005ee61
Martin Ansong <martin.ansong@harness.io>
2021-10-29 15:12:12 +0100

    add gitignore

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "app_server" {
+    bucket = "tf-test-bucket-%d"
+    acl    = "private"
+
+  tags = {
+    Name = "mansongBucket"
+    Environment = "Staging"
+  }
+}
